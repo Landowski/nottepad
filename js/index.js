697c25317c2f5b8bdfa2aafb9910cad3b24c830e
Landowski <105506160+Landowski@users.noreply.github.com>
2025-03-11 12:30:56 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -709,7 +709,7 @@ function createCookieConsent() {
 
     // Adiciona o conteúdo
     cookieConsent.innerHTML = `
-        <div class="cookie-header"><strong>Aviso sobre cookies</strong></div>
+        <div class="cookie-header"><strong>Cookie warning</strong></div>
         <p style="font-size: 13px; color: #555;">We use cookies to improve your experience. By continuing, you agree to our . <a href="/cookie-policy" target="_blank">Cookie Policy</a>.</p>
         <button onclick="acceptCookies()">I understand and accept</button>
     `;
